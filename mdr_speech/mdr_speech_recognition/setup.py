41cc6a133ebd3e041a8c975e505f2e597c19d377
robertocaiwu <roberto.cai@smail.inf.h-brs.de>
2018-10-25 17:33:16 +0200

    update python setup file to include Kaldi wrapper class

--- mdr_speech/mdr_speech_recognition/setup.py
+++ mdr_speech/mdr_speech_recognition/setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['mdr_speech_recognition'],
-   package_dir={'mdr_speech_recognition': 'ros/src/mdr_speech_recognition'}
+   packages=['SpeechRecognition', 'mdr_speech_recognition'],
+   package_dir={'SpeechRecognition': 'common/src/SpeechRecognition',
+                'mdr_speech_recognition': 'ros/src/mdr_speech_recognition'}
 )
 
 setup(**d)
